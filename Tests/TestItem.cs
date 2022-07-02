f2e32aaf6e5380a5ee82381f50c4b936db54728f
cdincer <dincercan@gmail.com>
2022-07-02 22:55:26 +0300

    Xunit added

---
+++ Tests/TestItem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Net6ShCart.Tests
+{
+    public class TestItem
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
